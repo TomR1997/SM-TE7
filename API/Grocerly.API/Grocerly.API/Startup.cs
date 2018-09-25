d1a05fcf6b7260d24d790d339a40c74529938e28
sanderdl <sanderdlart@gmail.com>
2018-09-25 14:52:08 +0200

    Servicestack startup

--- API/Grocerly.API/Grocerly.API/Startup.cs
+++ API/Grocerly.API/Grocerly.API/Startup.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Funq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using ServiceStack;
+using ServiceStack.Api.OpenApi;
+using ServiceStack.Auth;
+using ServiceStack.Caching;
 
 namespace Grocerly.API
 {
@@ -25,10 +30,62 @@ namespace Grocerly.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseServiceStack(new AppHost());
+
+            app.Run(context =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                context.Response.Redirect("/metadata");
+                return Task.FromResult(0);
             });
+
         }
     }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("EducationAttendence", typeof(SchoolServices).Assembly) { }
+
+        public override void Configure(Container container)
+        {
+            this.Plugins.Add(new OpenApiFeature());
+            Plugins.Add(new AuthFeature(() => new AuthUserSession(),
+            new IAuthProvider[] {
+                    new CredentialsAuthProvider(),
+                    new JwtAuthProvider(AppSettings)
+                    {
+                        AuthKeyBase64 = "dGVzdA==",
+                        RequireSecureConnection = false
+                    }
+            })
+            {
+                IncludeAssignRoleServices = false
+            });
+            Plugins.Add(new CorsFeature(
+                allowedHeaders: "Authorization",
+                allowedOrigins: "*"
+            ));
+
+
+            container.Register<ICacheClient>(new MemoryCacheClient());
+            var userRep = new InMemoryAuthRepository();
+            container.Register<IUserAuthRepository>(userRep);
+
+            var user = userRep.CreateUserAuth(new UserAuth
+            {
+                UserName = "test"
+            }, "Test123");
+
+            var admin = userRep.CreateUserAuth(new UserAuth
+            {
+                UserName = "Admin"
+            }, "Test123");
+
+            userRep.AssignRoles(user, new[] { "User" });
+
+            userRep.AssignRoles(admin, new[] { "User" });
+            userRep.AssignRoles(admin, new[] { "Admin" });
+        }
+    }
+
 }
