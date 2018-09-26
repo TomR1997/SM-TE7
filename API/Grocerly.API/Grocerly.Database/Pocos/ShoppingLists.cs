05322b53df2664bd05c6d3eaa5c30ec13e30f218
Laat,Sander A.P.A. de <s.delaat@student.fontys.nl>
2018-09-26 13:59:48 +0200

    Start of shoppinglist

---
+++ API/Grocerly.API/Grocerly.Database/Pocos/ShoppingLists.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grocerly.Database.Pocos
+{
+
+    public class ShoppingLists
+    {
+        public Guid Id { get; set; }
+    }
+}
