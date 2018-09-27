3293abd053cf868bc79f2096247d35162bd3dc2a
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-09-27 11:04:30 +0200

    Added controllers

---
+++ Grocerly.API/Grocerly.API/Controllers/ProductsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Grocerly.Database;
+using Grocerly.Database.Pocos;
+
+namespace Grocerly.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : Controller
+    {
+        private readonly GrocerlyContext _context;
+
+        public ProductsController(GrocerlyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<Products> GetProducts()
+        {
+            return _context.Products;
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProducts([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var products = await _context.Products.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(products);
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProducts([FromRoute] Guid id, [FromBody] Products products)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != products.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(products).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<IActionResult> PostProducts([FromBody] Products products)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Products.Add(products);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProducts", new { id = products.Id }, products);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProducts([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var products = await _context.Products.SingleOrDefaultAsync(m => m.Id == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(products);
+            await _context.SaveChangesAsync();
+
+            return Ok(products);
+        }
+
+        private bool ProductsExists(Guid id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Grocerly.API/Grocerly.API/Controllers/ShoppingListsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Grocerly.Database;
+using Grocerly.Database.Pocos;
+
+namespace Grocerly.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ShoppingLists")]
+    public class ShoppingListsController : Controller
+    {
+        private readonly GrocerlyContext _context;
+
+        public ShoppingListsController(GrocerlyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ShoppingLists
+        [HttpGet]
+        public IEnumerable<ShoppingLists> GetShoppingLists()
+        {
+            return _context.ShoppingLists;
+        }
+
+        // GET: api/ShoppingLists/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetShoppingLists([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var shoppingLists = await _context.ShoppingLists.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (shoppingLists == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(shoppingLists);
+        }
+
+        // PUT: api/ShoppingLists/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShoppingLists([FromRoute] Guid id, [FromBody] ShoppingLists shoppingLists)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != shoppingLists.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(shoppingLists).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShoppingListsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ShoppingLists
+        [HttpPost]
+        public async Task<IActionResult> PostShoppingLists([FromBody] ShoppingLists shoppingLists)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.ShoppingLists.Add(shoppingLists);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetShoppingLists", new { id = shoppingLists.Id }, shoppingLists);
+        }
+
+        // DELETE: api/ShoppingLists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShoppingLists([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var shoppingLists = await _context.ShoppingLists.SingleOrDefaultAsync(m => m.Id == id);
+            if (shoppingLists == null)
+            {
+                return NotFound();
+            }
+
+            _context.ShoppingLists.Remove(shoppingLists);
+            await _context.SaveChangesAsync();
+
+            return Ok(shoppingLists);
+        }
+
+        private bool ShoppingListsExists(Guid id)
+        {
+            return _context.ShoppingLists.Any(e => e.Id == id);
+        }
+    }
+}
