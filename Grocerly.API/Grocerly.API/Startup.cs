72dd6054e719f6c3f2c9e0755fb544d01be801a3
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-09-27 13:43:27 +0200

    Changed MY to Grocerly :angry:

--- Grocerly.API/Grocerly.API/Startup.cs
+++ Grocerly.API/Grocerly.API/Startup.cs
@@ -44,7 +44,7 @@ namespace Grocerly.API
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Grocerly API V1");
                 c.RoutePrefix = string.Empty;
             });
 
