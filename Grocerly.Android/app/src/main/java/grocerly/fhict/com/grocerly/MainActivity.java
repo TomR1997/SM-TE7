30b2e9cd9501329f4469ddf19746b3c12bfa52b4
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-10-25 11:50:26 +0200

    Revert "Test"
    This reverts commit 504d74fee8b36cc8e8a786431c18a5fcc8ea45b5.

--- Grocerly.Android/app/src/main/java/grocerly/fhict/com/grocerly/MainActivity.java
+++ Grocerly.Android/app/src/main/java/grocerly/fhict/com/grocerly/MainActivity.java
@@ -16,7 +16,6 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    //Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
