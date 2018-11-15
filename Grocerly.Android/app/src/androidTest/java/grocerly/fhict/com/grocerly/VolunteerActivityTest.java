dbdb88383c660a461e395d621dfd9b2071c02ade
sanderdl <sanderdlart@gmail.com>
2018-11-15 14:00:45 +0100

    add result

--- Grocerly.Android/app/src/androidTest/java/grocerly/fhict/com/grocerly/VolunteerActivityTest.java
+++ Grocerly.Android/app/src/androidTest/java/grocerly/fhict/com/grocerly/VolunteerActivityTest.java
@@ -1,5 +1,9 @@
 package grocerly.fhict.com.grocerly;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +15,7 @@ import androidx.test.filters.LargeTest;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
@@ -24,9 +29,17 @@ public class VolunteerActivityTest {
 
     @Test
     public void startBarcodeScannerTest(){
+
+        Intent resultData = new Intent();
+        String barcode = "12345678";
+        resultData.putExtra("Barcode", barcode);
+        Instrumentation.ActivityResult result =
+                new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+
         onView(withId(R.id.startBarcodeScanner))
                 .perform(click());
 
-        intended(toPackage("grocerly.fhict.com.grocerly"));
+        intending(toPackage("grocerly.fhict.com.grocerly"))
+            .respondWith(result);
     }
 }
