67eb94d15360454d8bae1f9bcb3ded455df80370
sanderdl <sanderdlart@gmail.com>
2018-11-28 11:22:09 +0100

    create iOS project

---
+++ Grocerly.iOS/Grocerly.iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Grocerly.iOS
+//
+//  Created by issd on 28/11/2018.
+//  Copyright © 2018 SM71. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
