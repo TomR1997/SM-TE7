9e3c1acda68784ec5cc77e2a9236fd4e556d4ec6
sanderdl <sanderdlart@gmail.com>
2018-12-12 12:42:30 +0100

    adjusted loader

--- Grocerly.iOS/Grocerly.iOS/extentions/DisplayLoader.swift
+++ Grocerly.iOS/Grocerly.iOS/extentions/DisplayLoader.swift
@@ -26,6 +26,13 @@ class DisplayLoader {
         container.center = onView.center
         container.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
         
+        let whiteBG = UIView()
+        whiteBG.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        whiteBG.center = onView.center
+        whiteBG.backgroundColor = UIColor(named: "Background")
+        whiteBG.layer.cornerRadius = 24
+        whiteBG.layer.masksToBounds = true
+        
         let animationView = LOTAnimationView(name: name)
         animationView.loopAnimation = true
         animationView.contentMode = .scaleAspectFill
@@ -35,7 +42,9 @@ class DisplayLoader {
             view.removeFromSuperview()
         }
         
+        container.addSubview(whiteBG)
         container.addSubview(animationView)
+        
         onView.addSubview(container)
         
         animationView.play()
